fe9d07928510e62406d78ab2748a9a8505d65058
ali <ali.j181@yahoo.com>
2025-06-01 21:31:21 +0330

    change the root of home page components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import Slider from "./Components/Home/slider/Slider";
-import Footer from "./Components/Home/Footer/Footer";
-import Header from "./Components/Home/Header/Header";
-import News from "./Components/Home/News/News";
+import Slider from "./Components/Pages/slider/Slider";
+import Footer from "./Components/Pages/Footer/Footer";
+import Header from "./Components/Pages/Header/Header";
+import News from "./Components/Pages/News/News";
 
 
 const App = ()=>{
   return(
     <section>
       <Header />
-
       <main>
-  
          <Slider />
          <News />
-        
       </main>
     <Footer/>
     </section>
